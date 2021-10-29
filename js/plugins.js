1159667271f2602d72be6dec980de2579605efc6
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2021-10-29 21:07:39 +0200

    .

---
+++ js/plugins.js
@@ -0,0 +1,11 @@
+let section = document.querySelector(".our-skills")
+let spans = document.querySelectorAll(".our-skils .progress-bar")
+
+window.onscroll = function (){
+    if (window.scrollY >= section.offsetTop){
+        console.log("helloooooooo");
+        spans.forEach((".progress-bar")) => {
+            spans.style.width = span.
+        }
+    }
+}
